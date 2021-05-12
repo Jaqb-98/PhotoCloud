48b039cdaecc031c1b3119d7ccf4964f7de57294
Jakub Abramczyk <jakub.abramczyk98@gmail.com>
2021-05-12 10:48:40 +0200

    Added Extended IdentityUser

--- PhotoCloud/Data/ApplicationDbContext.cs
+++ PhotoCloud/Data/ApplicationDbContext.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PhotoCloud.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PhotoCloud.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<PhotoModel> Photos { get; set; }
     }
 }
