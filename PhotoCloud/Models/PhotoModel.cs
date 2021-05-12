52c834122892f5b71026e5adb48e3eda8b2a953c
Jakub Abramczyk <jakub.abramczyk98@gmail.com>
2021-05-12 10:49:31 +0200

    Create PhotoModel.cs

---
+++ PhotoCloud/Models/PhotoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoCloud.Models
+{
+    public class PhotoModel
+    {
+        public string Id { get; set; }
+        public string PhotoName { get; set; }
+        public DateTime UploadDate { get; set; }
+    }
+}
