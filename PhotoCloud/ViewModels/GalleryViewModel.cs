c8edf73b6f5859b6007c90d9de47953b30ab09d5
Jakub Abramczyk <jakub.abramczyk98@gmail.com>
2021-05-12 10:50:39 +0200

    Create GalleryViewModel.cs

---
+++ PhotoCloud/ViewModels/GalleryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using PhotoCloud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoCloud.ViewModels
+{
+    public class GalleryViewModel
+    {
+
+        public ICollection<PhotoModel> Photos { get; set; }
+    }
+}
