76f38e1ebcb3894b737ff2409fa76e3de87e3312
Jakub Abramczyk <jakub.abramczyk98@gmail.com>
2021-05-12 10:50:07 +0200

    Create IPhotoService.cs

---
+++ PhotoCloud/Services/Interfaces/IPhotoService.cs
@@ -0,0 +1,12 @@
+﻿using PhotoCloud.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PhotoCloud.Services
+{
+    public interface IPhotoService
+    {
+        void AddPhotos(string userId, List<PhotoModel> photos);
+        Task<ICollection<PhotoModel>> GetUsersPhotos(string userId, int page = 0);
+    }
+}
